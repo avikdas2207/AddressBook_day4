6256aad6989d31445d1ee06ec3fb979c33758d9e
avikdas2207 <avikdas3535@gmail.com>
2020-11-12 01:25:49 +0530

    first commit

--- AddressBook.java
+++ AddressBook.java
@@ -12,17 +12,45 @@ public class AddressBook{
 		contacts[numOfContacts]=new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
 		numOfContacts++;
 	}
+	public void editContact(String name,String address,String city,String state, int zip,long phoneNumber,String email) {
+		for(int i=0;i<numOfContacts;i++) {
+			if(contacts[i]==null) {
+				continue;
+			}
+			if(name.equals(contacts[i].getFirstName()+" "+contacts[i].getLastName())) {
+				contacts[i].setAddress(address);
+				contacts[i].setCity(city);
+				contacts[i].setState(state);
+				contacts[i].setZip(zip);
+				contacts[i].setPhoneNumber(phoneNumber);
+				contacts[i].setEmail(email);
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
 		AddressBook addressBook=new AddressBook();
 		System.out.println("Enter the fields in order: \nfirst_name\nlast_name\naddress\ncity\nstate\nzip\nphone no.\nemail");
 		addressBook.addContact(sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),Integer.parseInt(sc.nextLine()),Long.parseLong(sc.nextLine()),sc.nextLine());
-		for(Contact contact:addressBook.contacts) {
+		System.out.println("Before edit:");
+		for(Contact contact: addressBook.contacts) {
 			if(contact==null) {
 				continue;
 			}
 			System.out.println(contact);
 		}
+		System.out.println("Enter name of person whose contact details are to be edited: ");
+		String name=sc.nextLine();
+		System.out.println("Enter the new fields in order: address\ncity\nstate\nzip\nphone no.\nemail");
+		addressBook.editContact(name,sc.nextLine(),sc.nextLine(),sc.nextLine(),Integer.parseInt(sc.nextLine()),Long.parseLong(sc.nextLine()),sc.nextLine());		
+		System.out.println("After Edit");
+		for(Contact contact: addressBook.contacts) {
+			if(contact==null) {
+				continue;
+			}
+			System.out.println(contact);
+		}
+		sc.close();
 	}	
 }
 
@@ -97,7 +125,7 @@ class Contact{
 	}
 	@Override
 	public String toString() {
-		return "Contact: "+firstName + " "+ lastName+ ", "+address+", "+city+", " +state+ ", "+", " +zip+ ", "+ phoneNumber + "\n"+email+".";
+		return "Contact: "+firstName + " "+ lastName+ ", "+address+", "+city+", " +state+ ", " +zip+ ", "+ phoneNumber + "\n"+email+".";
 	}
 	
 }
